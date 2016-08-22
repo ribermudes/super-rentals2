00a566c66adaf0e170d26f8407b48067fb514805
Alex Jacks and Rachael Bermudes <rachael@>
2016-08-22 10:16:59 -0700

    adds country display to index page for cities and rentals

--- app/models/rental.js
+++ app/models/rental.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   owner: DS.attr(),
   city: DS.belongsTo('city', { async: true }),
+  country: DS.attr(),
   type: DS.attr(),
   image: DS.attr(),
   bedrooms: DS.attr(),
